111a96e1aa15b979423cc7b90436633f0d506054
AgustinChavero <agustindanielchavero@gmail.com>
2024-11-24 23:32:10 -0300

    update - routes

--- routes/api.php
+++ routes/api.php
@@ -8,15 +8,15 @@ Route::prefix('/external')->group(function () {
     Route::prefix('/auth')->group(function () {
         Route::post('/register', [AuthController::class, 'register']);
         Route::post('/login', [AuthController::class, 'login']);
-        Route::post('/reset-password', [AuthController::class, 'reset']);
-        Route::post('/verify-email', [AuthController::class, 'verifyEmail']);
+        Route::put('/modify-password', [AuthController::class, 'modify']);
     });
 });
 
 # Rutas protegidas
 Route::prefix('/internal')->middleware('auth:api')->group(function () {
     Route::prefix('/auth')->group(function () {
+        Route::post('/verify-email', [AuthController::class, 'verifyEmail']);
         Route::post('/logout', [AuthController::class, 'logout']);
-        Route::post('/modify-password', [AuthController::class, 'modifyPassword']);
+        Route::post('/reset-password', [AuthController::class, 'reset']);
     });
 });
